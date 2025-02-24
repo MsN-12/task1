00877777bf6679f620185a6d34efc952d27ae266
Mohsen <msn@DESKTOP-3P4LK41.>
2025-02-24 22:40:04 +0330

    init commit7

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter(),
-    paths: {
-      base:
-        process.env.NODE_ENV === "production" ? "/sveltekit-github-pages" : "",
-    },
   },
 };
+
+export default config;
